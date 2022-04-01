335a73d17986efc6793e3a487a6e82a3c5723ade
Sheng Kai <55977910+ShenggKai@users.noreply.github.com>
2022-04-01 16:40:15 +0700

    Add files via upload

---
+++ BoundaryFillandScanLineFill/header/main.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <cmath> 
+#include <stack>
+#include "graphics.h"
+#include "Lab3.h"
+
+using namespace std;
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
